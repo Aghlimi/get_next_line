6e8af7ae13df79889c479ae2cc4ebb7a088b74aa
Ahmed Ghlimi <aghlimi@c3r4p10.1337.ma>
2024-11-01 22:17:02 +0100

    finished

--- get_next_line.c
+++ get_next_line.c
@@ -3,85 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aghlimi <aghlimi@student.42.fr>            +#+  +:+       +#+        */
+/*   By: aghlimi <aghlimi@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 19:47:24 by aghlimi           #+#    #+#             */
-/*   Updated: 2024/11/01 21:40:41 by aghlimi          ###   ########.fr       */
+/*   Updated: 2024/11/01 22:15:54 by aghlimi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#define BUFFER_SIZE 1024
+#include "get_next_line.h"
 
-static int	ft_str_len(const char *text)
+char	*ft_read(int fd, char *buffer, char *result)
 {
-	int	i;
+	char	*xd3ndkblati;
+	char	*xd3ndkblati7tanta;
 
-	i = 0;
-	if (text)
-		while (text[i])
-			i++;
-	return (i);
-}
-static char	*ft_addchar(char *text, char c)
-{
-	int		i;
-	char	*result;
-
-	result = ft_calloc(ft_str_len(text) + 2, 1);
-	if (result == NULL)
-		return (NULL);
-	i = 0;
-	if (text)
+	while (read(fd, buffer, BUFFER_SIZE) > 0)
 	{
-		ft_memmove(result, text, ft_str_len(text));
-		free(text);
-	}
-	result[ft_str_len(result)] = c;
-	return (result);
-}
-
-char	*ft_tonl(char *text)
-{
-	int		i;
-	char	*result;
-
-	i = 0;
-	result = NULL;
-	while (text[i])
-	{
-		result = ft_addchar(result, text[i]);
+		xd3ndkblati = result;
+		xd3ndkblati7tanta = ft_tonl(buffer);
+		if (!xd3ndkblati7tanta)
+			return (free(xd3ndkblati), NULL);
+		result = ft_strjoin(result, xd3ndkblati7tanta);
 		if (!result)
-			return (NULL);
-		if (text[i++] == 10)
-			return (result);
+			return (free(xd3ndkblati7tanta), free(xd3ndkblati), NULL);
+		free((free(xd3ndkblati), xd3ndkblati7tanta));
+		if (ft_strchr(result, 10))
+			return (ft_memmove(buffer, buffer + firstnl(buffer),
+					ft_strlen(buffer) + 1), result);
 	}
 	return (result);
-}
-
-int	firstnl(char *text)
-{
-	int	i;
-
-	i = 0;
-	while (text[i])
-	{
-		if (text[i++] == 10)
-			return (i);
-	}
-	return (-1);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	buffer[BUFFER_SIZE + 1];
 	char		*result;
-	char		*xd3ndkblati;
-	char		*xd3ndkblati7tanta;
 
 	result = NULL;
 	if (ft_strlen(buffer))
@@ -92,38 +48,6 @@ char	*get_next_line(int fd)
 		ft_memmove(buffer, buffer + firstnl(buffer), ft_strlen(buffer) + 1);
 	}
 	if ((result && !ft_strchr(result, 10)) || !result)
-		while (read(fd, buffer, BUFFER_SIZE) > 0)
-		{
-			xd3ndkblati = result;
-			xd3ndkblati7tanta = ft_tonl(buffer);
-			if (!xd3ndkblati7tanta)
-				return (free(xd3ndkblati), NULL);
-			result = ft_strjoin(result, xd3ndkblati7tanta);
-			if (!result)
-				return (free(xd3ndkblati7tanta), free(xd3ndkblati), NULL);
-			free((free(xd3ndkblati),xd3ndkblati7tanta));
-			if (ft_strchr(result, 10))
-				return (ft_memmove(buffer, buffer + firstnl(buffer),
-						ft_strlen(buffer) + 1), result);
-		}
+		result = ft_read(fd, buffer, result);
 	return (result);
-}
-int	main(int argc, char const *argv[])
-{
-	char	*line;
-	int		fd;
-
-	fd = open("test.txt", O_RDONLY);
-	if (fd < 0)
-	{
-		perror("fd");
-	}
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	while ((line = get_next_line(fd)) != NULL)
-	{
-		printf("%s", line);
-		free(line);
-	}
-	return (0);
 }
---
+++ get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aghlimi <aghlimi@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/01 21:54:05 by aghlimi           #+#    #+#             */
+/*   Updated: 2024/11/01 22:15:20 by aghlimi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include "libft.h"
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+int		ft_str_len(const char *text);
+char	*ft_addchar(char *text, char c);
+char	*ft_tonl(char *text);
+int		firstnl(char *text);
+
+#endif
